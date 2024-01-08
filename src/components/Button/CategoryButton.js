cc91396ddadaba8add663fddb2634d9f7816c4b3
Dobbymin <gmin0701@knu.ac.kr>
2024-01-09 01:51:08 +0900

    [Feat]: 페이지 이동 추가

--- src/components/Button/CategoryButton.js
+++ src/components/Button/CategoryButton.js
@@ -1,13 +1,92 @@
 import Stack from '@mui/material/Stack';
 import { Button } from '@mui/material';
+import CallPage from '../../pages/CallPage';
 
 const CategoryButton = () => {
     return (
-        <Stack>
-            <Button>국물류</Button>
-            <Button>안주류</Button>
-            <Button>주류</Button>
-            <Button>직원호출</Button>
+        <Stack spacing={2}>
+            <Stack spacing={2}>
+                <Button
+                    href="soup"
+                    sx={{
+                        color: 'black',
+                        backgroundColor: 'white',
+                        borderColor: 'black',
+                        width: '8.5rem',
+                        height: '3rem',
+                        borderRadius: 3,
+                        fontSize: '1rem',
+
+                        ':hover': {
+                            color: 'black',
+                            borderColor: 'black',
+                            backgroundColor: '#FFC700',
+                        },
+                    }}
+                >
+                    국물류
+                </Button>
+            </Stack>
+            <Button
+                href="snack"
+                sx={{
+                    color: 'black',
+                    backgroundColor: 'white',
+                    borderColor: 'black',
+                    width: '8.5rem',
+                    height: '3rem',
+                    borderRadius: 3,
+                    fontSize: '1rem',
+
+                    ':hover': {
+                        color: 'black',
+                        borderColor: 'black',
+                        backgroundColor: '#FFC700',
+                    },
+                }}
+            >
+                안주류
+            </Button>
+            <Button
+                href="drink"
+                sx={{
+                    color: 'black',
+                    backgroundColor: 'white',
+                    borderColor: 'black',
+                    width: '8.5rem',
+                    height: '3rem',
+                    borderRadius: 3,
+                    fontSize: '1rem',
+
+                    ':hover': {
+                        color: 'black',
+                        borderColor: 'black',
+                        backgroundColor: '#FFC700',
+                    },
+                }}
+            >
+                주류
+            </Button>
+            <Button
+                href="call"
+                sx={{
+                    color: 'black',
+                    backgroundColor: 'white',
+                    borderColor: 'black',
+                    width: '8.5rem',
+                    height: '3rem',
+                    borderRadius: 3,
+                    fontSize: '1rem',
+
+                    ':hover': {
+                        color: 'black',
+                        borderColor: 'black',
+                        backgroundColor: '#FFC700',
+                    },
+                }}
+            >
+                직원호출
+            </Button>
         </Stack>
     );
 };
