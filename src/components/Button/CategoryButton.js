fe880c44e088ce7ac4c844c39e51f21ee178b3b9
Dobbymin <gmin0701@knu.ac.kr>
2024-01-08 23:51:28 +0900

    [Feat]: 카테고리 버튼 추가

--- src/components/Button/CategoryButton.js
+++ src/components/Button/CategoryButton.js
@@ -1,5 +1,15 @@
+import Stack from '@mui/material/Stack';
+import { Button } from '@mui/material';
+
 const CategoryButton = () => {
-    return <div>CategoryButton</div>;
+    return (
+        <Stack>
+            <Button>국물류</Button>
+            <Button>안주류</Button>
+            <Button>주류</Button>
+            <Button>직원호출</Button>
+        </Stack>
+    );
 };
 
 export default CategoryButton;
