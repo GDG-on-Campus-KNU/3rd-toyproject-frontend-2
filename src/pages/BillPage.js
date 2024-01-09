68fbdd6e0a6518a639735ebbf4f0fcaa52b224e4
leejooyeonn <julia2071ljy@naver.com>
2024-01-09 15:51:28 +0900

    [style]: 주류, 계산서 페이지 스타일

--- src/pages/BillPage.js
+++ src/pages/BillPage.js
@@ -1,5 +1,196 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
+import Paper from '@mui/material/Paper';
+
+import "./BackgroundColor.css";
+
+import tag from "../assets/tag.png"
+
+const billBoxStyle = {
+    bgcolor: '#ffffff', 
+    height: '77vh', 
+    borderRadius: '15px', 
+    padding: '30px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start', 
+    justifyContent: 'flex-start',
+};
+
+const titleBoxStyle = {
+    display: 'flex', 
+    alignItems: 'center',
+    marginBottom: '10px',
+    width: '100%'
+};
+
+const closeButtonStyle = {
+    borderRadius: '50px', 
+    bgcolor: '#000000', 
+    '&:hover': { bgcolor: '#808080' },
+    marginLeft: 'auto',
+}
+
+const numButtonStyle = {
+    borderRadius: '50px', 
+    bgcolor: '#000000', 
+    '&:hover': { bgcolor: '#808080' },
+}
+
+const tagImgStyle = {
+    width: '50px', 
+    height: '50px', 
+    marginRight: '20px',
+}
+
+const h4Style = {
+    color: '#ffc700', 
+    margin: 5,
+    fontWeight: 'bold',
+}
+
+const h5Style = {
+    color: '#ffc700', 
+    margin: 5,
+    fontWeight: 'bold',
+    marginLeft: 'auto',
+}
+
+const line1Style = {
+    borderTop: '5px solid #ffc700', 
+    width: '100%', 
+    marginY: '10px',
+}
+
+const line2Style = {
+    borderTop: '1px solid #000000', 
+    width: '100%',
+}
+
+const gridContainerStyle = {
+    display: 'grid',
+    gridTemplateColumns: '7fr 2fr 1fr 2fr',
+    gap: '10px',
+};
+
+const textCellStyle = {
+    padding: '20px',
+};
+
+const paperBoxStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    border: '1px solid #000',
+    borderRadius: '50px',
+    padding: '5px',
+    width: '100%',
+    marginBottom: '15px',
+    marginTop: '15px',
+};
+
 const BillPage = () => {
-    return <div>BillPage</div>;
+    return (
+        <Container fixed sx={{ m: 9 }}>
+            <Grid container>
+                <Grid item xs={2}>
+                    CategoryBar
+                </Grid>
+                <Grid item xs={10}>
+                    <Box sx={ billBoxStyle }>
+                        
+                        <Box sx={ titleBoxStyle }>
+                            <img src={tag} style={ tagImgStyle } />
+                            <Typography variant="h4" style={ h4Style }>계산서</Typography>
+                            <Button variant="contained" sx={ closeButtonStyle }>
+                                X 닫기
+                            </Button>
+                        </Box>
+                        <Box sx={ line1Style } />
+
+                        <Paper elevation={0} sx={{width: '100%', minHeight: '70%'}}>
+                            <Box sx={gridContainerStyle}>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1" style={{fontWeight:'bold'}}>직화무뼈닭발</Typography>
+                                </Box>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1">23,000원</Typography>
+                                </Box>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1">1개</Typography>
+                                </Box>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1">23,000원</Typography>
+                                </Box>
+                            </Box>
+                            <Box sx={ line2Style } />
+
+                            <Box sx={gridContainerStyle}>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1" style={{fontWeight:'bold'}}>순두부찌개</Typography>
+                                </Box>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1">12,000원</Typography>
+                                </Box>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1">1개</Typography>
+                                </Box>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1">12,000원</Typography>
+                                </Box>
+                            </Box>
+                            <Box sx={ line2Style } />
+
+                            <Box sx={gridContainerStyle}>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1" style={{fontWeight:'bold'}}>참이슬</Typography>
+                                </Box>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1">5,000원</Typography>
+                                </Box>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1">2개</Typography>
+                                </Box>
+                                <Box sx={textCellStyle}>
+                                    <Typography variant="body1">10,000원</Typography>
+                                </Box>
+                            </Box>
+                            <Box sx={ line2Style } />
+                        </Paper>
+
+                        <Box sx={ line1Style } />
+                        
+                        <Grid container spacing={2} sx={{minHeight: '15%'}}>
+                            <Grid item xs={8}>
+                                <Paper elevation={0} sx={{ height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                                    <Box sx={paperBoxStyle}>
+                                        <Typography variant="body1" style={{ marginRight: '10px' }}>더치페이</Typography>
+                                        <Button variant="contained" sx={numButtonStyle}>
+                                            +
+                                        </Button>
+                                        <Typography variant="body1" style={{ margin: '0 10px' }}>3</Typography>
+                                        <Button variant="contained" sx={numButtonStyle}>
+                                            -
+                                        </Button>
+                                        <Typography variant="body1" style={{ marginLeft: '10px' }}>1인당 15,000원</Typography>
+                                    </Box>
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Paper elevation={0} sx={{ minHeight: '100% ', display: 'flex', alignItems: 'center'}}>
+                                    <Typography variant="h5" style={h5Style}>합계 45,000원</Typography>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 };
 
 export default BillPage;
--- src/pages/DrinkPage.js
+++ src/pages/DrinkPage.js
@@ -1,5 +1,50 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+import MenuCard from "../components/Card/MenuCard"
+
+import terra from "../assets/terra.png"
+import cass from "../assets/cass.png"
+import chamiseul from "../assets/chamiseul.png"
+import jinlo from "../assets/jinlo.png"
+import chungha from "../assets/chungha.png"
+import highball from "../assets/highball.png"
+
+import "./BackgroundColor.css";
+
+
 const DrinkPage = () => {
-    return <div>DrinkPage</div>;
+    return (
+        <Container fixed sx={{ m: 9}}>
+            <Grid container spacing={10}>
+                <Grid item xs={2}>
+                    CategoryBar
+                </Grid>
+                <Grid item xs={10}>
+                    <Grid container spacing={5}>
+                        <Grid item xs={4}>
+                            <MenuCard image={terra} menu="테라" price="4,500"/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <MenuCard image={cass} menu="카스" price="4,500"/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <MenuCard image={chamiseul} menu="참이슬" price="5,000"/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <MenuCard image={jinlo} menu="진로" price="5,000"/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <MenuCard image={chungha} menu="별빛 청하" price="6,000"/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <MenuCard image={highball} menu="산토리 하이볼" price="7,000"/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 };
 
 export default DrinkPage;
