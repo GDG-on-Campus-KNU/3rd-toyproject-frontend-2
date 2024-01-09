414233736d3f2d8643bb678f13d596ca0ff64f44
Dobbymin <gmin0701@knu.ac.kr>
2024-01-09 21:33:57 +0900

    [Feat]: 전체적인 위치 수정

--- src/pages/DrinkPage.js
+++ src/pages/DrinkPage.js
@@ -14,8 +14,8 @@ import './BackgroundColor.css';
 
 const DrinkPage = () => {
     return (
-        <Container fixed sx={{ m: 9 }}>
-            <Grid container spacing={10}>
+        <Container maxWidth="lg">
+            <Grid container marginY="1px" spacing={4}>
                 <Grid item xs={2}>
                     CategoryBar
                 </Grid>
