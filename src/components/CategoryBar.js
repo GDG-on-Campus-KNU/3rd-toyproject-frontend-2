1ff373f557b063bdd6b56447c82c345142d16b42
Dobbymin <gmin0701@knu.ac.kr>
2024-01-12 01:21:32 +0900

    [Feat]: 버튼 색 유지 기능 구현

--- src/components/CategoryBar.js
+++ src/components/CategoryBar.js
@@ -2,17 +2,16 @@ import Stack from '@mui/material/Stack';
 import CategoryButton from './Button/CategoryButton';
 
 const CategoryBar = (category) => {
-
     const getColor = (currentCategory) => {
         return category.category === currentCategory ? 'selected' : 'default';
     };
 
     return (
-        <Stack spacing={3} direction='column'>
-            <CategoryButton category='국물류' color={getColor('soup')} />
-            <CategoryButton category='안주류' color={getColor('snack')} />
-            <CategoryButton category='주류' color={getColor('drink')} />
-            <CategoryButton category='직원호출' color={getColor('call')} />
+        <Stack spacing={2.5} direction="column">
+            <CategoryButton page="soup" category="국물류" color={getColor('soup')} />
+            <CategoryButton page="snack" category="안주류" color={getColor('snack')} />
+            <CategoryButton page="drink" category="주류" color={getColor('drink')} />
+            <CategoryButton page="call" category="직원호출" color={getColor('call')} />
         </Stack>
     );
 };
