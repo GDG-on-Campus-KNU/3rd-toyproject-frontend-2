9a011bd4a9f81d5c742fc62e89760fcb7cf1cdcf
Dobbymin <gmin0701@knu.ac.kr>
2024-01-13 16:19:25 +0900

    [Style]: CallPage Style 재사용 가능하게 수정

---
+++ src/styles/CallPageStyle.js
@@ -0,0 +1,31 @@
+export const orderGridStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    backgroundColor: '#ffffff',
+};
+
+export const buttonTextStyle1 = {
+    textAlign: 'center',
+    padding: '0.2rem',
+};
+
+export const buttonStyle1 = {
+    color: '#ffc700',
+    border: '#ffc700 1px solid',
+
+    height: '2rem',
+    marginX: '1rem',
+};
+
+export const buttonStyle2 = {
+    marginX: '1rem',
+    marginY: '0.25rem',
+};
+
+export const lineStyle = {
+    borderTop: '1px solid #000000',
+    alignItem: 'center',
+    width: '3rem',
+    marginY: '2rem',
+};
