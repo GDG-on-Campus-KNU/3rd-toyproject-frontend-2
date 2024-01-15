183abc0aa8c98e036328f86a65ca5eb91bc3c64b
Junsu Heo <gjwnstn515@naver.com>
2024-01-15 18:58:08 +0900

    [Feat] : CartPage에 주문하기 버튼 기능 구현(상품추가 완성돼야 확인가능)

--- src/components/Button/OrderButton.js
+++ src/components/Button/OrderButton.js
@@ -1,4 +1,6 @@
 import Button from "@mui/material/Button";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const orderButtonStyle = {
   color: "black",
@@ -10,8 +12,28 @@ const orderButtonStyle = {
 const OrderButton = (props) => {
   const { payment } = props;
 
+  const OrderClick = () => {
+    alert(payment + "로 결제한다.");
+
+    // ㅡㅡㅡㅡㅡㅡ여기는 백엔드랑 프론트 기능 완성되면 확인가능ㅡㅡㅡㅡㅡ
+    // const getPosts = () => {
+    //   axios
+    //     .post("http://35.208.58.86:8080/api/payment", {
+    //       paymentMethod: payment,
+    //     })
+    //     .then((res) => {
+    //       console.log("zzz");
+    //       alert("주문이 완료되었습니다!");
+    //     });
+    // };
+
+    // useEffect(() => {
+    //   getPosts();
+    // }, []);
+  };
+
   return (
-    <Button variant="contained" sx={orderButtonStyle}>
+    <Button variant="contained" onClick={OrderClick} sx={orderButtonStyle}>
       주문하기
     </Button>
   );
