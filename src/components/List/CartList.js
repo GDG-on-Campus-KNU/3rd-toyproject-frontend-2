65a305f192eb57c75fbe99e8e9d6503a3c94bcbf
Junsu Heo <gjwnstn515@naver.com>
2024-01-15 22:50:01 +0900

    [Feat]: CardList 컴포넌트화(상품 담아서 확인 필요)

--- src/components/List/CartList.js
+++ src/components/List/CartList.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -46,40 +48,51 @@ const cartButtonStyle = {
 };
 
 const CartList = () => {
+  const [lists, setLists] = useState([]);
+
+  const getPosts = () => {
+    axios.get("http://35.208.58.86:8080/api/cart").then((res) => {
+      console.log(res.data);
+      setLists(res.data);
+    });
+  };
+
   return (
     <Paper elevation={0} sx={{ width: "100%", minHeight: "70%" }}>
       {/* 컴포넌트화하기ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */}
-      <Box>
-        <Box sx={textCellStyle}>
-          <Typography variant="body1" style={{ fontWeight: "bold" }}>
-            1 참이슬
-          </Typography>
+      {lists.map((list) => (
+        <Box>
+          <Box sx={textCellStyle}>
+            <Typography variant="body1" style={{ fontWeight: "bold" }}>
+              {list.cartItems[0].menuId} {list.cartItems[0].menuName} 1 참이슬
+            </Typography>
 
-          <Button variant="contained" sx={cartDeleteButtonStyle}>
-            X
-          </Button>
+            <Button variant="contained" sx={cartDeleteButtonStyle}>
+              X
+            </Button>
+          </Box>
+
+          <Box sx={textCellStyle}>
+            <Button variant="contained" sx={cartButtonStyle}>
+              +
+            </Button>
+            <Typography
+              variant="body1"
+              style={{ marginLeft: 17, marginRight: 17 }}
+            >
+              {list.cartItems[0].amount}개
+            </Typography>
+            <Button variant="contained" sx={cartButtonStyle}>
+              -
+            </Button>
+
+            <Typography variant="body1" style={priceStyle}>
+              가격 {list.cartItems[0].menuPrice}원
+            </Typography>
+          </Box>
+          <Box sx={line2Style} />
         </Box>
-
-        <Box sx={textCellStyle}>
-          <Button variant="contained" sx={cartButtonStyle}>
-            +
-          </Button>
-          <Typography
-            variant="body1"
-            style={{ marginLeft: 17, marginRight: 17 }}
-          >
-            2개
-          </Typography>
-          <Button variant="contained" sx={cartButtonStyle}>
-            -
-          </Button>
-
-          <Typography variant="body1" style={priceStyle}>
-            가격 10,000원
-          </Typography>
-        </Box>
-      </Box>
-      <Box sx={line2Style} />
+      ))}
 
       {/* <Box>
         <Box sx={textCellStyle}>
@@ -110,8 +123,8 @@ const CartList = () => {
             가격 12,000원
           </Typography>
         </Box>
+        <Box sx={line2Style} />
       </Box>
-      <Box sx={line2Style} />
 
       <Box>
         <Box sx={textCellStyle}>
@@ -142,8 +155,9 @@ const CartList = () => {
             가격 23,000원
           </Typography>
         </Box>
-      </Box>
-      <Box sx={line2Style} /> */}
+        <Box sx={line2Style} />
+      </Box> */}
+
       {/* 컴포넌트화하기ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */}
     </Paper>
   );
