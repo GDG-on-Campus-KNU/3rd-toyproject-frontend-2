9d7b11a59417655746467b4c7db87ea2fa568920
Dobbymin <gmin0701@knu.ac.kr>
2024-01-16 17:22:36 +0900

    [Style]: 주문목록 스타일 코드 정리

--- src/components/OrderContainer.js
+++ src/components/OrderContainer.js
@@ -1,19 +1,21 @@
-import { Box, Button, Container, Grid, Typography } from '@mui/material';
+import { Button, Grid, Typography } from '@mui/material';
 import Minus from './Button/CallButton/Minus';
 import Plus from './Button/CallButton/Plus';
-import { buttonStyle1, buttonStyle2, buttonTextMarginStyle, lineStyle } from '../styles/CallPageStyle';
+import { buttonStyle1, buttonStyle2, gridStyle1, lineStyle } from '../styles/CallPageStyle';
 
-const OrderContainer = ({ children }) => {
+const OrderContainer = ({ order, number }) => {
     return (
         <>
-            <Grid item display="flex" marginTop="1rem" marginX="1rem">
-                <Typography sx={buttonStyle2}>숟가락</Typography>
+            <Grid item sx={gridStyle1}>
+                <Typography sx={buttonStyle2}>{order}</Typography>
                 <Plus />
-                <Typography sx={buttonStyle2}>3개</Typography>
+                <Typography sx={buttonStyle2}>{number}개</Typography>
                 <Minus />
                 <Button sx={buttonStyle1}>삭제</Button>
             </Grid>
-            <Box sx={lineStyle} alignContent="center" />
+            <Grid item>
+                <Typography sx={lineStyle} alignContent="center" />
+            </Grid>
         </>
     );
 };
