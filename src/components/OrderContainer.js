7c7dbcf28a18cc822a084584b54e7c30507d81b4
Dobbymin <gmin0701@knu.ac.kr>
2024-01-19 09:44:33 +0900

    [Style]: 전체적인 위치 재설정

--- src/components/OrderContainer.js
+++ src/components/OrderContainer.js
@@ -1,22 +1,39 @@
 import { Button, Grid, Typography } from '@mui/material';
+
+import { buttonStyle1, buttonStyle2, gridStyle1, lineStyle } from '../styles/CallPageStyle';
 import Minus from './Button/CallButton/Minus';
 import Plus from './Button/CallButton/Plus';
-import { buttonStyle1, buttonStyle2, gridStyle1, lineStyle } from '../styles/CallPageStyle';
 
-const OrderContainer = ({ order, number }) => {
+const OrderContainer = () => {
+    // const dispatch = useDispatch();
+    // // const requestCheck = (id, name, mount) => {
+    // //     axios.get('http://35.208.58.86:8080/api/customer/request', {
+    // //         id: '1',
+    // //         name: '숫가락',
+    // //         amount: '1',
+    // //     });
+    // // };
+
+    // const orderCounter = useSelector((state) => state.call.amount);
+    // const [counter, setCounter] = useState(0);
+
+    // useEffect(() => {
+    //     dispatch(setCounter(counter));
+    // }, [dispatch, counter]);
+
     return (
-        <>
-            <Grid item sx={gridStyle1}>
-                <Typography sx={buttonStyle2}>{order}</Typography>
+        <Grid container flexDirection="column">
+            <Grid item sx={gridStyle1} justifyContent="flex-end">
+                <Typography sx={buttonStyle2}>아아아아</Typography>
                 <Plus />
-                <Typography sx={buttonStyle2}>{number}개</Typography>
+                <Typography sx={buttonStyle2}>1개</Typography>
                 <Minus />
                 <Button sx={buttonStyle1}>삭제</Button>
             </Grid>
             <Grid item>
                 <Typography sx={lineStyle} alignContent="center" />
             </Grid>
-        </>
+        </Grid>
     );
 };
 
